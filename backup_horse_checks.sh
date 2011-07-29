79b9d32f5e0b731b58c5b4346e4c113a578d32c8
George Redinger <george@georgeredinger.com>
2011-07-28 19:17:34 -0700

    use pipelines to avoid string too long errors

--- backup_horse_checks.sh
+++ backup_horse_checks.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 #assumes backup_competitions.sh has been run
-for year in `ls | egrep -oh -e 19.* -e 20.*`
-do
+OLD_IFS="$IFS";
+# Character used for splitting
+IFS=" ";
+mkdir horses
+
+ls | egrep -oh -e "^19.*" -e "^20.*" | 
+while read year;do
   echo -n > horse_checks.tmp
-  echo -n > horse_checks.sh
-	for competition in `ls $year/*.html`
-	do
+  echo -n > horse_checks.srt
+	ls $year/*.html | 
+	while read competition;do
     competition_number=$(echo `basename $competition` | tr -cd '[[:digit:]]')
+		echo  -ne "$competition_number\r"
     grep -oh  "http://www.usdf.org/scorecheck/ScoreCheckResults.asp?NumberPass=[0-9]*" "$competition" |
 		while read url;do
       horse_number=$(echo $url | tr -cd '[[:digit:]]')
-      echo  "echo  $horse_number; wget -T 7  --tries=10 --retry-connrefused -O \"$year/horse-check-$year-$competition_number-$horse_number.html\"  \"$url\"" >> horse_checks.tmp
+      echo  "$horse_number $url" >> horse_checks.tmp
 		done
-    sort -u horse_checks.tmp > horse_checks.sh
-    sh horse_checks.sh
+  done
+	echo
+    sort -u horse_checks.tmp > horse_checks.srt
+		cat horse_checks.srt | while read line;do
+		 a=( $line ); 
+		 for i in {1..10};do
+			 echo ${a[0]}
+		   wget --quiet -O "horses/${a[0]}.html" "${a[1]}"
+		    if [ "$?" == "0" ];then
+					break
+				fi	
+	  	   echo  " retry  ${a[0]}"
+	   done
 	done
 done
 
